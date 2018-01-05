6a16b52839953c17ef8a560c83450f3a9c600848
Abhishek Jee <abhishek.jee.2411@gmail.com>
2018-01-05 17:07:06 +0530

    Updated

--- pilot.py
+++ pilot.py
@@ -1,1 +1,6 @@
 from textblob import TextBlob
+
+wiki = TextBlob("Python is a high-level, general-purpose programming language.")
+
+# Part-Of-Speech tagging
+wiki.tags [('Python','NNP'),('is','VBZ'),('a','DT'),('high-level','JJ'),('general-purpose','JJ'),('programming','NN'),('language','NN')
